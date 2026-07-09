9ba5490c04b61228b6eb53552aef1015ad62f7da
Eduardo Morales <emoral435@gmail.com>
2026-07-09 10:09:19 -0500

    feat: make tabs default in nvim

--- home/.config/nvim/lua/vim_config.lua
+++ home/.config/nvim/lua/vim_config.lua
@@ -1,5 +1,9 @@
 local o = vim.opt
 vim.g.mapleader = ' '          -- space is the leader key
+o.tabstop = 2                  -- a tab displays as 2 columns
+o.shiftwidth = 2               -- indent/unindent by 2 columns
+o.softtabstop = 2              -- tab key inserts 2 columns
+o.expandtab = false            -- use real tab characters, not spaces
 o.number = true                -- absolute number on the cursor line, relative elsewhere
 o.relativenumber = true        -- relative line numbers for fast jumps
 o.ignorecase = true            -- search is case-insensitive by default
